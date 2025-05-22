dd7483a5ace634e558ed8b92cc690607408cfdb8
gubitty <renato.thomaz21@gmail.com>
2025-05-22 09:55:29 -0300

    Estrutura Básica

---
+++ app/index.tsx
@@ -0,0 +1,46 @@
+import { Text, TouchableOpacity } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+export default function index() {
+  const nome = "Renato";
+
+  return <View>
+    <Text>Jogo da Velha</Text>
+    <Text>Vencedor</Text>
+    <View>
+        <View>
+            <TouchableOpacity>
+                <Text>X</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>X</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>X</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity>
+                <Text>X</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>X</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>X</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity>
+                <Text>X</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>X</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>X</Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+  </View>
+}
